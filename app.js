2d08b8f53cf9cc49e713428e7457f05f9d112832
Benjamin Bowser <bowserbl@miamioh.edu>
2021-03-13 16:41:54 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+	res.render("index");
+	//response.send("<h1>Coming Soon!</h1>");
+});
+
+app.get("/btc/:page", function(req, res) {
+        var page = req.params.page;
+	res.send("<h1>page: " + page + "</h1>"); // Send Hello World
+});
+
+
+// Set the application to listen on port 80
+app.listen(80, function() {
+	console.log("Application has started on port 80"); // Log console message
+});
